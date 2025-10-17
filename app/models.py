0228697bbab23bd5ddb6c4bc1a58ae8fb9769f11
Kushi Numdin <numdinkushi@gmail.com>
2025-10-17 07:41:23 +0100

    feat: users can vote

--- app/models.py
+++ app/models.py
@@ -30,3 +30,11 @@ class User(Base):
     updated_at = Column(TIMESTAMP(timezone=True),
                         nullable=False, server_default=text('now()'))
     posts = relationship("Post", back_populates="owner")
+
+class Vote(Base):
+    __tablename__ = "votes"
+
+    user_id = Column(Integer, ForeignKey(
+        "users.id", ondelete="CASCADE"), primary_key=True)
+    post_id = Column(Integer, ForeignKey(
+        "posts.id", ondelete="CASCADE"), primary_key=True)
